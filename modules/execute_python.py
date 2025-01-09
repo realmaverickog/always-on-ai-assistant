d9adbe4eb15a5142100aa4aadde5122259e3a4af
qwukker <minor7addfloortom@gmail.com>
2025-01-09 15:27:16 -0600

    progress

--- modules/execute_python.py
+++ modules/execute_python.py
@@ -2,18 +2,15 @@ import subprocess
 import shlex
 
 
-def execute_uv_python(command: str, file_path: str = "") -> str:
+def execute_uv_python(command: str, file_path: str) -> str:
     """Execute the tests and return the output as a string."""
-    complete_command = f"uv run {command}"
-
-    if file_path:
-        complete_command = f"uv run python {file_path} {command}"
+    complete_command = f"uv run python {file_path} {command}"
 
     return execute(complete_command)
 
 
 def execute(command: str) -> str:
-    """Execute the tests and return the output as a string."""
+    """Execute shell code and return the output as a string."""
     result = subprocess.run(
         shlex.split(command),
         capture_output=True,
